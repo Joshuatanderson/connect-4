578f5634f77c149007986dabad9973d5246d9330
joshuatanderson <joshuatanderson1@gmail.com>
2019-03-12 16:19:51 -0500

    adds comments to plan next dev steps

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,14 +2,14 @@ import React, {Component} from 'react';
 import Board from './Board';
 import Header from './Header';
 
-    //Game (states: height, width, squares (an array))
-        //Header (no props or states needed)
-        //Board (props: height, width) 
-            //Row ()
-                //Square(squares(index))
-                    //Marker (props: squares(index))
-
-
+    //to add:
+        //lift redIsNext and circles to the game component.
+        //create a function that stops you from replacing an existing piece
+        //add a function that checks to see if someone won after a piece is placed
+        //add a function that triggers a winner effect
+        //add a new game button that resets the states to start over
+        //active player functional component (should contain player names, and circles that toggle on and off when the active player state changes)
+        //
 
 class Game extends Component {
     state = {
